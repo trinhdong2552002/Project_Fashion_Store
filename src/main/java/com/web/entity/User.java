45021211f9ed3bb502b50e808fa47f51d27c0922
nam <nthanhnam0821@gmail.com>
2025-11-11 09:56:51 +0700

    fix: edit user profile

--- src/main/java/com/web/entity/User.java
+++ src/main/java/com/web/entity/User.java
@@ -1,6 +1,56 @@
+// package com.web.entity;
+
+// import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+// import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+// import com.web.enums.UserType;
+// import lombok.Getter;
+// import lombok.Setter;
+// import org.hibernate.annotations.BatchSize;
+
+// import javax.persistence.*;
+// import java.sql.Date;
+// import java.sql.Time;
+// import java.util.*;
+
+// @Entity
+// @Table(name = "users")
+// @Getter
+// @Setter
+// public class User{
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     @Column(name = "id")
+//     private Long id;
+
+//     private String username;
+
+//     private String email;
+
+//     private String password;
+
+//     private String fullname;
+
+//     private String phone;
+
+//     private Boolean actived;
+
+//     private String activation_key;
+
+//     private Date createdDate;
+
+//     private String tokenFcm;
+
+//     @Enumerated(EnumType.STRING)
+//     private UserType userType;
+
+//     @ManyToOne
+//     @JoinColumn(name = "authority_name")
+//     private Authority authorities;
+// }
+
 package com.web.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.web.enums.UserType;
 import lombok.Getter;
@@ -9,14 +59,13 @@ import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.sql.Date;
-import java.sql.Time;
 import java.util.*;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-public class User{
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -47,5 +96,12 @@ public class User{
     @ManyToOne
     @JoinColumn(name = "authority_name")
     private Authority authorities;
-}
 
+    // 🟢 Thêm mới hai trường để lưu thông tin tài khoản người dùng
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "birthdate")
+    // 
+    private java.sql.Date birthdate;
+}
